022cbeab7a33c132185bae05b851f4be12c7f1d0
BruceChen18 <1071495011@qq.com>
2019-11-18 23:06:35 +0800

    add bubble sort

---
+++ src/main/java/com/tuobo/sort/Bubble.java
@@ -0,0 +1,27 @@
+package com.tuobo.sort;
+
+
+public class Bubble {
+
+    public static void sort(int[] arr) {
+        int tmp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+}
